bfe62545db3483a59dd3295380f872a538575f56
Adam <adam_mikulas@mckinsey.com>
2024-01-17 21:01:56 +0100

    feat: add project setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+dotenv.config({ path: __dirname + "/.env.local" });
+
+const port = process.env.PORT ?? 3000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/movies", async (req: Request, res: Response) => {
+  return res.json({ message: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`server running on localhost:${port}`);
+});
